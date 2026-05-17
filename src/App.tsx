a37c637e5ca07b7e2d5181b4062e93d6b6c96c25
Sovanna Ra <sovann.ra277@gmail.com>
2026-05-17 14:22:27 +0700

    Fix browser tab title - force JongTinh title

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Refine, Authenticated } from "@refinedev/core";
+import { RefineComponentProps, Refine, Authenticated } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider, ErrorComponent, ThemedLayout, ThemedTitle, AuthPage } from "@refinedev/antd";
@@ -181,7 +181,15 @@ function AppContent() {
             </Routes>
             <RefineKbar />
             <UnsavedChangesNotifier />
-            <DocumentTitleHandler />
+            <DocumentTitleHandler
+                 handler={({ action, resource }) => {
+                        const siteName = "JongTinh";
+                        if (resource?.meta?.label) {
+                            return `${resource.meta.label} | ${siteName}`;
+                        }
+                        return `${siteName} — Online Shopping Cambodia`;
+                }}
+            />
         </Refine>
     );
 }
